0e30724bcf689ab5fed5a781b719b56322ec67c3
MPIO1002 <minhphuc309@gmail.com>
2024-04-04 18:27:18 +0700

    update

--- src/view/ExamInfo.java
+++ src/view/ExamInfo.java
@@ -1,3 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;

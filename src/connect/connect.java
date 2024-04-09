34d473efd835fc9d48335e8ddc349ecf64527caf
MyPhungg <myphunglop71@gmail.com>
2024-04-09 22:51:52 +0700

    Phan cua Phung

--- src/connect/connect.java
+++ src/connect/connect.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package connect;
-import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+//import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import java.sql.*;
 /**
  *

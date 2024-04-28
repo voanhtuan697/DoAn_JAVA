7cb2793eb8b518a6c1eaa670617b96db6575bfaf
unknown <minhphuc309@gmail.com>
2024-04-28 15:14:11 +0100

    add writePDF

---
+++ src/helper/writePDF.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helper;
+
+/**
+ *
+ * @author Minh Phuc
+ */
+import com.itextpdf.text.Chunk;
+import java.awt.Desktop;
+import java.awt.FileDialog;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.util.Date;
+import javax.swing.JButton;
+
+public class writePDF {
+
+    SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/YYYY HH:mm");
+    Document document = new Document();
+    FileOutputStream file;
+    JFrame jf = new JFrame();
+    FileDialog fd = new FileDialog(jf, "In pdf", FileDialog.SAVE);
+    Font fontNormal10;
+    Font fontBold15;
+    Font fontBold25;
+    Font fontBoldItalic15;
+
+    public writePDF() {
+        try {
+            fontNormal10 = new Font(BaseFont.createFont("lib/TimesNewRoman/SVN-Times New Roman.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED), 12, Font.NORMAL);
+            fontBold25 = new Font(BaseFont.createFont("lib/TimesNewRoman/SVN-Times New Roman Bold.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED), 25, Font.NORMAL);
+            fontBold15 = new Font(BaseFont.createFont("lib/TimesNewRoman/SVN-Times New Roman Bold.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED), 15, Font.NORMAL);
+            fontBoldItalic15 = new Font(BaseFont.createFont("lib/TimesNewRoman/SVN-Times New Roman Bold Italic.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED), 15, Font.NORMAL);
+        } catch (DocumentException | FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException ex) {
+            Logger.getLogger(writePDF.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void chooseURL(String url) {
+        try {
+            document.close();
+            document = new Document();
+            file = new FileOutputStream(url);
+            PdfWriter writer = PdfWriter.getInstance(document, file);
+            document.open();
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Khong tim thay duong dan file " + url);
+        } catch (DocumentException ex) {
+            JOptionPane.showMessageDialog(null, "Khong goi duoc document !");
+        }
+    }
+
+    public void setTitle(String title) {
+        try {
+            Paragraph pdfTitle = new Paragraph(new Phrase(title, fontBold25));
+            pdfTitle.setAlignment(Element.ALIGN_CENTER);
+            document.add(pdfTitle);
+            document.add(Chunk.NEWLINE);
+        } catch (DocumentException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private String getFile(String name) {
+        fd.pack();
+        fd.setSize(800, 600);
+        fd.validate();
+        Rectangle rect = jf.getContentPane().getBounds();
+        double width = fd.getBounds().getWidth();
+        double height = fd.getBounds().getHeight();
+        double x = rect.getCenterX() - (width / 2);
+        double y = rect.getCenterY() - (height / 2);
+        Point leftCorner = new Point();
+        leftCorner.setLocation(x, y);
+        fd.setLocation(leftCorner);
+        fd.setFile(name);
+        fd.setVisible(true);
+        String url = fd.getDirectory() + fd.getFile();
+        if (url.equals("null")) {
+            return null;
+        }
+        return url;
+    }
+
+    private void openFile(String file) {
+        try {
+            File path = new File(file);
+            Desktop.getDesktop().open(path);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static Chunk createWhiteSpace(int length) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(" ");
+        }
+        return new Chunk(builder.toString());
+    }
+}
+
